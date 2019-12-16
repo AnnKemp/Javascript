d6c8f0cc19aec7b782c71a70f3e1785c681f0994
AnnKemp <ann.lemp@scarlet.be>
2019-12-16 10:18:07 +0100

    de derde oef

---
+++ oef3/script3.js
@@ -0,0 +1,9 @@
+(function myFunction() {
+        var txt;
+        if (confirm("Do you want cake?")) {
+            txt = "Congrats!";
+        } else {
+            txt = "More cake for me then :p !";
+        }
+       alert(txt);
+})();
