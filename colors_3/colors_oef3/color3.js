5ab7fb8835f3345618949567038585199b3c0252
AnnKemp <ann.lemp@scarlet.be>
2019-12-17 09:43:48 +0100

    de derde oefening op kleur

---
+++ colors_3/colors_oef3/color3.js
@@ -0,0 +1,11 @@
+
+
+document.getElementById("run").addEventListener("click", function(){
+
+    getal1=Math.floor(Math.random() * 10);
+    getal2=Math.floor(Math.random() * 10);
+    getal3=Math.floor(Math.random() * 10);
+    var achtergrondkleur="#"+getal1+getal2+getal3;
+    document.body.style.backgroundColor = achtergrondkleur;
+});
+
