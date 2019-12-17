a18fd31bddec6a8c6419de1918a6737cf2c04692
AnnKemp <ann.lemp@scarlet.be>
2019-12-17 10:08:45 +0100

    tweede en deze keer werkende versie van de oefening

--- maths_02/oef2/math2.js
+++ maths_02/oef2/math2.js
@@ -1,46 +1,56 @@
-var teken;
 
-var plus = document.getElementById("addition").innerText;
-var min=document.getElementById("substraction").innerText;
+
+var pluske = document.getElementById("addition").innerText;
+var minneke=document.getElementById("substraction").innerText;
 var maal=document.getElementById("multiplication").innerText;
 var delen=document.getElementById("division").innerText;
-// dus de tekens worden zonder problemen hierboven opgehaald, dit heb ik getest met een alert();
 
-document.getElementById("addition").addEventListener("click", rekenen(plus));
-document.getElementById("substraction").addEventListener("click", rekenen(min));
-document.getElementById("multiplication").addEventListener("click", rekenen(maal));
-document.getElementById("division").addEventListener("click", rekenen(delen));
 
 function rekenen(teken) {
-    alert("de functie wordt aangeroepen"+teken);
+
     var resultaat;
     var waardeEen = document.getElementById("op-one").value;
     var waardeTwee = document.getElementById("op-two").value;
-    var terug= document.getElementById("calcul").innerHTML;
-    terug="Het resultaat is: ";
+
+    var terug="Het resultaat is: ";
 
     switch (teken) {
         case "+":
             resultaat = (parseInt(waardeEen) + parseInt(waardeTwee));
             terug+=resultaat;
-            alert(resultaat);
+
             break;
         case "-":
             resultaat = (parseInt(waardeEen) - parseInt(waardeTwee));
             terug+=resultaat;
-            alert(terug);
+
             break;
-        case "/":
+        case "÷":
             resultaat = (parseInt(waardeEen) / parseInt(waardeTwee));
             terug+=resultaat;
-            alert(terug);
+
             break;
-        case "*":
+        case "×":
             resultaat = (parseInt(waardeEen) * parseInt(waardeTwee));
             terug+=resultaat;
-            alert(terug);
+
             break;
         default:
             terug = "En wat wou je berekenen . . . ?";
     }
+    document.getElementById("calcul").innerHTML=terug;
 }
+
+document.getElementById("addition").addEventListener("click", function(){
+    rekenen(pluske);
+});
+document.getElementById("multiplication").addEventListener("click", function(){
+    rekenen(maal);
+});
+document.getElementById("substraction").addEventListener("click", function(){
+    rekenen(minneke);
+});
+document.getElementById("division").addEventListener("click", function(){
+    rekenen(delen);
+});
+
