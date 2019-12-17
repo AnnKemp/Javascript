554d3ea7f24ada287e2e36e1e480e2d5000a3c98
AnnKemp <ann.lemp@scarlet.be>
2019-12-17 09:25:42 +0100

    de eerste oefening op kleuren

--- colors_3/colors_oef1/color1.js
+++ colors_3/colors_oef1/color1.js
@@ -8,9 +8,20 @@
 
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
+/*
+document.getElementById("myBtn").addEventListener("click", function(){
+    document.getElementById("demo").innerHTML = "Hello World";
+}); */
 
-(function() {
-
-// your code here
-
-})();
+document.getElementById("red").addEventListener("click", function() {
+    document.body.style.backgroundColor = "red";
+});
+document.getElementById("green").addEventListener("click", function() {
+    document.body.style.backgroundColor = "green";
+});
+document.getElementById("yellow").addEventListener("click", function() {
+    document.body.style.backgroundColor = "yellow";
+});
+document.getElementById("blue").addEventListener("click", function() {
+    document.body.style.backgroundColor = "blue";
+});
