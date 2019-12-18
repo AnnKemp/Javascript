b6cd3e02bb0180ef524380d4feda80b5a7d1cbc6
AnnKemp <ann.lemp@scarlet.be>
2019-12-18 17:06:36 +0100

    even kleuroef 3 upgraden vanwege een betere oplossing

--- colors_3/colors_oef3/color3.js
+++ colors_3/colors_oef3/color3.js
@@ -2,11 +2,13 @@
 
 document.getElementById("run").addEventListener("click", function(){
 
-    getal1=Math.floor(Math.random() * 10);
-    getal2=Math.floor(Math.random() * 10);
-    getal3=Math.floor(Math.random() * 10);
-    var achtergrondkleur="#"+getal1+getal2+getal3;
-    document.body.style.backgroundColor = achtergrondkleur;
+    var letter="0123456789ABCDEF";
+    var color="#";
+
+    for(var i=0; i<6;i++){
+        color+=letter[Math.floor(Math.random()*16)];
+    }
+    document.body.style.backgroundColor = color;
 });
 
 })();
