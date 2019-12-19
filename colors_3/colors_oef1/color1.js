5ac952c96e82de81351d0dab7c5af8df5ec89eaf
AnnKemp <ann.lemp@scarlet.be>
2019-12-19 10:32:20 +0100

    toch nog in een oefening een haakje teveel gevonden

--- colors_3/colors_oef1/color1.js
+++ colors_3/colors_oef1/color1.js
@@ -1,20 +1,6 @@
-/* becode/javascript
- *
- * /03-colors/01-change-bcg-one/script.js - 3.1: couleur de fond (1)
- *
- * coded by leny@BeCode
- * started at 26/10/2018
- */
 
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
-/*
-document.getElementById("myBtn").addEventListener("click", function(){
-    document.getElementById("demo").innerHTML = "Hello World";
-}); */
-(function() {
 
-}
+
 document.getElementById("red").addEventListener("click", function() {
     document.body.style.backgroundColor = "red";
 });
@@ -28,4 +14,3 @@ document.getElementById("blue").addEventListener("click", function() {
     document.body.style.backgroundColor = "blue";
 });
 
-})();
