fcb6610b30655d7b552d6200c7b3fec470d144f7
AnnKemp <ann.lemp@scarlet.be>
2019-12-19 10:02:36 +0100

    vulling voor de map

--- dates_4/oef1/date_1.js
+++ dates_4/oef1/date_1.js
@@ -1,18 +1,20 @@
-/* becode/javascript
- *
- * /04-dates/01-title-by-hour-one/script.js - 4.1: texte en fonction de l'heure (1)
- *
- * coded by leny@BeCode
- * started at 26/10/2018
- */
 
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
 
 (function() {
+    //if it's before 18h
+var day="Hello";
+var evening="Good evening";
 
-    // to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
+    var datum = new Date();
+    var uur = datum.getHours();
 
-    // your code here
+    if(uur >= 18) {
+
+        document.getElementsByTagName("p")[1].innerHTML = evening;
+
+    }else{
+        document.getElementsByTagName("p")[1].innerHTML = day;
+
+    }
 
 })();
