e610b05efa3b3593b360d2be808e2d4bdbc6b53d
AnnKemp <ann.lemp@scarlet.be>
2019-12-19 15:21:28 +0100

    second ex finished

--- arrays_5/oef2_array/array_2.js
+++ arrays_5/oef2_array/array_2.js
@@ -5,12 +5,12 @@
  * coded by leny@BeCode
  * started at 26/10/2018
  */
-/*When you click the button, do the following transformations to the array <strong>fruits</strong>:
+/*    do the following transformations to the array <strong>fruits</strong>:
 remove the first and the last element, add an element "banana" at the start of the array,
     and add an element "kiwi" at the end. */
 
 (function() {
-
+    document.getElementById("run").addEventListener("click", function() {
     var fruits = [
         "pomme",
         "poire",
@@ -23,7 +23,14 @@ remove the first and the last element, add an element "banana" at the start of t
         "raisin",
         "cerise",
     ];
+// shift removes the first element "pomme" from fruits
+        fruits.shift();
+fruits.unshift("banana");    // Adds a new element "banana" to fruits
+// pop removes the last element from an array
+        fruits.pop();
 
-    // your code here
-
+// push adds a new element ("Kiwi") to fruits at the end
+        fruits.push("Kiwi");
+        alert(fruits);
+    });
 })();
