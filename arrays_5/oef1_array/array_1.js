e761d40334bd4ffb415d73021933bfc9ed842e32
AnnKemp <ann.lemp@scarlet.be>
2019-12-19 14:53:41 +0100

    eerste oefening af

---
+++ arrays_5/oef1_array/array_1.js
@@ -0,0 +1,24 @@
+
+(function() {
+    document.getElementById("run").addEventListener("click", function() {
+        var fruits = [
+            "apple",
+            "pear",
+            "raspberry",
+            "tomatos",
+            "kiwi",
+            "banana",
+            "orange",
+            "mandarin",
+            "durian",
+            "peache",
+            "grap",
+            "cherry",
+        ];
+//When you click the button, display the fourth item from the array <strong>fruits</strong>
+    //in the console.
+        console.log(fruits[3]);
+});
+
+})();
+
