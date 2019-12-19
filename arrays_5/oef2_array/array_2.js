d1aa74e7f9a2940d4f761b163ff1c1fb7234eb2f
AnnKemp <ann.lemp@scarlet.be>
2019-12-19 14:58:33 +0100

    tweede map toevoegen

---
+++ arrays_5/oef2_array/array_2.js
@@ -0,0 +1,29 @@
+/* becode/javascript
+ *
+ * /05-arrays/02-manipulate-array/script.js - 5.2: manipulation de tableau
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+/*When you click the button, do the following transformations to the array <strong>fruits</strong>:
+remove the first and the last element, add an element "banana" at the start of the array,
+    and add an element "kiwi" at the end. */
+
+(function() {
+
+    var fruits = [
+        "pomme",
+        "poire",
+        "fraise",
+        "tomate",
+        "orange",
+        "mandarine",
+        "durian",
+        "pêche",
+        "raisin",
+        "cerise",
+    ];
+
+    // your code here
+
+})();
