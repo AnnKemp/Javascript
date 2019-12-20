d9e8c7342b363cb0fb986af040ef203dbb6dbe9e
AnnKemp <ann.lemp@scarlet.be>
2019-12-20 12:53:06 +0100

    make new folder and files

---
+++ arrays_5/oef11_array/array_11.js
@@ -0,0 +1,32 @@
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function() {
+
+    document.getElementById("run").addEventListener("click", function() {
+
+    var fruits = [
+        "cerise",
+        "durian",
+        "pomme",
+        "poire",
+        "fraise",
+        "tomate",
+        "orange",
+        "mandarine",
+        "fraise",
+        "durian",
+        "pêche",
+        "cerise",
+        "raisin",
+        "cerise",
+    ];
+    /*
+<p>When you click the button, remove the duplicates from the array <strong>fruits</strong>
+ and display the resulting array in the console.
+    Each value should be there one time, and one time only.</p>$/   */
+
+    // your code here
+
+});
+})();
