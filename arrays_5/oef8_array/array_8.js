bfafbc06df7bda0d22b2d74ff17141d1a176d36f
AnnKemp <ann.lemp@scarlet.be>
2019-12-20 14:12:36 +0100

    exc array 8 is finished!

--- arrays_5/oef8_array/array_8.js
+++ arrays_5/oef8_array/array_8.js
@@ -17,20 +17,17 @@
     ];
 /*When you click the button, log in the console if,
     yes or no, there is an <em>apple</em> in the array <strong>fruits</str */
-    // your code here
 
         // zie https://www.w3schools.com/jsref/jsref_find.asp
 
-        var result;
-// moet nog uitzoeken hoe het met die fruit-waarde zit die moet worden doorgegeven . . .
-        function checkApple(fruit) {
+for (i=0;fruits.length;i++){
 
-        if(fruit == "apple"){
-           return result="yes";
+        if(fruits[i] == "apple"){
+            console.log("yes");
+            break;
         }else{
-           return result="no";
+            console.log("no");
         }
-        console.log(fruits.find(checkApple));
-        }
+}
 });
 })();
