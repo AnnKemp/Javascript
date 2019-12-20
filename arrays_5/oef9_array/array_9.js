dd1b24c8d66b24dcf4948b48b89bb2cb6a75235a
AnnKemp <ann.lemp@scarlet.be>
2019-12-20 14:34:21 +0100

    first version, halfway not finished yet

--- arrays_5/oef9_array/array_9.js
+++ arrays_5/oef9_array/array_9.js
@@ -2,6 +2,12 @@
 
     document.getElementById("run").addEventListener("click", function() {
 
+        var numberss=[1,2,3,4,5,6,7,8,9,10];
+
+       for(i=0;i<=numberss.length;i++) {
+            var list = document.getElementsByTagName("tr")[0];
+            list.getElementsByTagName("td")[i].innerHTML = numberss[i];
+        }
     /*<p>When you click the button, display the ten elements of an array of
         10 numbers (between 1 and 100) in the table. Then display in the definition list:
             the smallest number, the biggest number, the sum of all numbers, and their average.</p>*/
