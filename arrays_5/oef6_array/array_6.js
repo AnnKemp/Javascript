50c1702112eed5a21ec5a614f5485862a288fe22
AnnKemp <ann.lemp@scarlet.be>
2019-12-20 12:02:02 +0100

    aanmaak map + step 1 of the exc

---
+++ arrays_5/oef6_array/array_6.js
@@ -0,0 +1,98 @@
+(function() {
+
+    document.getElementById("run").addEventListener("click", function() {
+
+    var people = [
+        {
+            firstname: "Bradford",
+            lastname: "Coldbath",
+            email: "bcoldbath0@wired.com",
+        },
+        {
+            firstname: "Nadiya",
+            lastname: "Stendell",
+            email: "nstendell1@nsw.gov.au",
+        },
+        {
+            firstname: "Auroora",
+            lastname: "Stapford",
+            email: "astapford2@wsj.com",
+        },
+        {
+            firstname: "Berkley",
+            lastname: "McKall",
+            email: "bmckall3@about.me",
+        },
+        {
+            firstname: "Wolf",
+            lastname: "McCurley",
+            email: "wmccurley4@yale.edu",
+        },
+        {
+            firstname: "Jorrie",
+            lastname: "Canedo",
+            email: "jcanedo5@engadget.com",
+        },
+        {
+            firstname: "Bethanne",
+            lastname: "Ackred",
+            email: "backred6@imgur.com",
+        },
+        {
+            firstname: "Zorah",
+            lastname: "Whild",
+            email: "zwhild7@sogou.com",
+        },
+        {
+            firstname: "Jean",
+            lastname: "Dupont",
+            email: "jdupont@elpais.com",
+        },
+        {
+            firstname: "Zulema",
+            lastname: "Ericsson",
+            email: "zericsson9@ed.gov",
+        },
+        {
+            firstname: "Brady",
+            lastname: "Scrannage",
+            email: "bscrannagea@google.fr",
+        },
+        {
+            firstname: "Isidore",
+            lastname: "Korf",
+            email: "ikorfb@google.com",
+        },
+        {
+            firstname: "Bartholomew",
+            lastname: "Stockbridge",
+            email: "bstockbridgec@is.gd",
+        },
+        {
+            firstname: "Laney",
+            lastname: "O' Mara",
+            email: "lomarad@forbes.com",
+        },
+        {
+            firstname: "Gabe",
+            lastname: "Keatch",
+            email: "gkeatche@google.fr",
+        },
+    ];
+        /* When you click the button, display in the console the mail adress of the person in the array <strong>people</g>
+      whose lastname is
+       "Dupont" and  firstname is "Jean". Then display his <em>index</em> in this array. */
+
+    people.forEach(myFunction);
+
+    function myFunction(item, index) {
+
+        if(("Jean" == item.firstname)&&("Dupont" == item.lastname)){
+            var mail=item.email;
+            var person="My firstname is Jean and my lastname is Dupont, my email is "+mail;
+        }
+        console.log(person);
+    }
+});
+
+})();
