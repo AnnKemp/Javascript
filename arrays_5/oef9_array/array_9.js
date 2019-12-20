f8b9d6c9d3c6eeb0f04698b88f4d4615a3e5224c
AnnKemp <ann.lemp@scarlet.be>
2019-12-20 12:51:57 +0100

    make new base folders

---
+++ arrays_5/oef9_array/array_9.js
@@ -0,0 +1,12 @@
+(function() {
+
+    document.getElementById("run").addEventListener("click", function() {
+
+    /*<p>When you click the button, display the ten elements of an array of
+        10 numbers (between 1 and 100) in the table. Then display in the definition list:
+            the smallest number, the biggest number, the sum of all numbers, and their average.</p>*/
+
+
+
+});
+})();
