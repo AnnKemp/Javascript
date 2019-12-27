6f371e665e81edc76c0f809f5ca3cb5bafb743e2
Ann Kemp <33941085+AnnKemp@users.noreply.github.com>
2019-12-27 12:44:34 +0100

    finished version of exc four

--- DOM/oef4/dom_4.js
+++ DOM/oef4/dom_4.js
@@ -1,13 +1,29 @@
 (function() {
+   /* . Create a new image
+                element inside the target element that displays the image referred to by the attribute value. Then remove
+                the original element.
+           <section class="material">
+            <span id="source" data-image="http://www.randomkittengenerator.com/cats/rotator.php"></span>
+            <figure id="target"></figure>
+        </section>
+        */
 
- var x = document.getElementsByTagName("span")[0].getAttributeNode("data-image").value;
+var x = document.getElementById("source");
+var waarde=x.getAttributeNode("data-image").value;
+// tot hiertoe werkt het
 
-    var beeld = document.createElement("img");
-    var attribuut = document.createAttribute("src");
-    beeld.appendChild(attribuut);
+var beeld = document.createElement("IMG");
+beeld.setAttribute("src", waarde);
+beeld.setAttribute("height", "300");
+beeld.setAttribute("width", "300");
+beeld.setAttribute("border", "5");
+beeld.setAttribute("alt", "kitten");
 
-    var element = document.getElementById("target");
-    element.appendChild(beeld);
-    document.setAttribute("src").value=x;
+var element = document.getElementById("target");
+element.appendChild(beeld);
+
+// het element verwijderen zonder de parent bij naam te kennen of te selecteren
+var item = document.getElementById("source");
+item.parentNode.removeChild(item);
 
 })();
