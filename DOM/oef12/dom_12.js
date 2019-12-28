e5ab3143489b64a76fe5a496838695887d68c657
Ann Kemp <33941085+AnnKemp@users.noreply.github.com>
2019-12-28 20:31:30 +0100

    Most recent version

--- DOM/oef12/dom_12.js
+++ DOM/oef12/dom_12.js
@@ -1,5 +1,4 @@
-//<p>Listen for changes in the input field and display "ok" in the validity span if the input is at least
-//8 characters long and contains at least two numbers.</p>
+
 
 (function() {
     var i=0;
@@ -9,19 +8,18 @@
         e = e || window.event;
         i+=1;
            if(i>=8) {
-               let waarde = document.getElementById("pass-one").value;
-               let patt =/^[a-z0-9_]{6,}[0-9]{2,}$/i;
+               /*
+               let patter =/^[a-z0-9_]{8, }$/i;
+               var testen = waarde.match(patter); // function match()*/
+                   let waarde = document.getElementById("pass-one").value;
+                   var patt = /\d{2}/g;  // pattern: contains 2 digits                   if(!pattern.test(text.value))
+                   var result = waarde.match(patt);
 
-               //var patt = /\d{2}/g;  // pattern: contains 2 digits
-               // minimum acht alfanumerische karakters    /^[a-z0-9_]{6,}[0-9]{2,}$/i;     if(!pattern.test(text.value))
-
-               var result = waarde.match(patt); // function match() toepassen
-                // het testen op de pattern gaat goed voor de cijfers misschien best ook de hoeveelheid in de pattern steken
-               if (result) {
-                   document.getElementById("validity").innerText = "ok";
-               } else {
-                   document.getElementById("validity").innerText = "min 8 characters and contains at least two numbers";
+                   if (result) {
+                       document.getElementById("validity").innerText = "ok";
+                   } else {
+                       document.getElementById("validity").innerText = "min 8 characters and contains at least two numbers";
+                   }
                }
-           }
     });
 })();
