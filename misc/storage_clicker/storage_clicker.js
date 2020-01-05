39ae99e2c102c195917e253c94cfb683326d5508
AnnKemp <ann.lemp@scarlet.be>
2020-01-05 21:27:00 +0100

    final version storage clicker

--- misc/storage_clicker/storage_clicker.js
+++ misc/storage_clicker/storage_clicker.js
@@ -7,28 +7,23 @@ display it) when the page is reloaded.
 // https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_win_localstorage2
 
 (function() {
-
     document.getElementById("increment").addEventListener("click", function () {
-
         // 1) checken of local storage door de browser ondersteund wordt
     if (typeof(Storage) !== "undefined") {
-
         let storag = localStorage.getItem("teller");
 
         if (storag){
-            counter = localStorage.getItem("teller"); // ok deze onthoud wél maar voor hoelang zou oneindig moeten zijn maar lijkt me toch niet zo . . .
-            //alert(counter);
+            counter = localStorage.getItem("teller");
         } else {
             let counter = 0;
-            // item om data lokaal op te slaan aanmaken en vullen
-            localStorage.setItem("teller", counter);
         }
-            parseInt(counter); // normaal zou ie hier  toch een getal van maken om dan hieronder op te tellen en dan te vertonen?! mmm da's voor morgen . . . iets maakt een string van dat getal...
+           counter=parseInt(counter);
             counter+= 1;
-        // item om data lokaal op te slaan/te vullen? nog even checken of dit de juiste verwoording is?!  en hoe zit dat met die read-only?!!!???
+
+        //  de nieuwe waarde opslagen in de localstorage
             localStorage.setItem("teller", counter);
-            // de stand tonen aan de user van de website
-            document.getElementById("target").innerHTML = counter;
+            // én de stand tonen aan de user van de website
+       document.getElementById("target").innerHTML= counter;
     }else {
         document.getElementById("target").innerHTML = "Sorry, your browser does not support Web Storage...";
     }
