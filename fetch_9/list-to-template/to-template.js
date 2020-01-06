0e4286885f6325e3273a0d25400373e3b23541d8
AnnKemp <ann.kemp@scarlet.be>
2020-01-06 16:59:27 +0100

    exc. 2 halfway

---
+++ fetch_9/list-to-template/to-template.js
@@ -0,0 +1,52 @@
+(() => {
+/*  <p>When you click on the button, use the fetch function, retrieve the list of X-Men and display it in the tag whose id is "target".
+   Use the template tag to reproduce an html structure for each hero.</p>
+   The template tag already exists in the HTML structure, be sure to check the HTML code!
+        <template id="tpl-hero">
+                <li class="hero">
+                    <h4 class="title">
+                        <strong class="name"></strong>
+                        <em class="alter-ego"></em>
+                    </h4>
+                    <p class="powers"></p>
+                </li>
+            </template>
+            <ol id="target"></ol>
+   */
+    document.getElementById("run").addEventListener("click", function(){
+
+        fetch("http://localhost:12345/list-to-console_1/api.json")
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                // de data er uit halen
+            data.heroes.forEach(thisFunction);
+
+                function thisFunction(item, index) {
+                   // console.log(item, index);
+                    // Get <template> element
+                    var itm = document.getElementById("tpl-hero");
+
+// Copy the <template element and its child nodes
+                    var cln = itm.content.cloneNode(true);
+
+// Append the cloned <template> element to <ol> with id="target"
+                    document.getElementById("target").appendChild(cln);
+// nu nog de data toewijzen
+
+                   // document.querySelector(".name")=item.name;
+                }
+
+
+            //           tag.selector=hero.eigenschap;
+            // query selectors
+
+        });
+        // het werkt ik snap er een beetje van . . .
+    });
+})();
+
+
+
+
